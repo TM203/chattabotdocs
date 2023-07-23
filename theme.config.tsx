c4fa17ed58d6c3e94c417420ddad01970a696842
TM203 <jtmayer@outlook.com>
2023-07-23 22:49:04 +1000

    updated theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,42 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Chattabot</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://chattabot.xyz',
+    icon: (
+      <img src='https://www.chattabot.xyz/_next/image?url=%2Ffavicon.ico&w=64&q=75' alt='Chattabot' width="35" height="35"/>
+    ),
+
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/uNumk3KMXw',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© Chattabot',
   },
+  docsRepositoryBase: 'https://github.com/TM203/chattabotdocs/tree/main',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Chattabot'
+    }
+  },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Chattabot Docs" />
+      <meta property="og:description" content="Learn more about chattabot!" />
+    </>
+  ),
+  logoLink: 'https://www.chattabot.xyz/',
+
+  nextThemes: {
+    defaultTheme: 'dark'
+    
+  },
+
+
+  primaryHue: { dark: 182,  light: 193}
 }
 
 export default config
